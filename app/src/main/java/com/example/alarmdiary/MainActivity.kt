35d1ee5c145d5919d78a4e16aa75d942495754be
LDongGyu <ehdrbchd@naver.com>
2020-05-10 22:20:59 +0900

    navigation drawer 적용

--- app/src/main/java/com/example/alarmdiary/MainActivity.kt
+++ app/src/main/java/com/example/alarmdiary/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.alarmdiary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,25 @@ class MainActivity : AppCompatActivity() {
                 "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
             startActivity(intent)
         }
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 홈 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.menu_hamber) // 홈 버튼 이미지
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+//        val actionBar = supportActionBar
+//        actionBar?.setDisplayShowTitleEnabled(false)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{
+                drawer_layout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun permissionGrantred(): Boolean {
