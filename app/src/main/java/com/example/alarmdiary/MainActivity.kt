2bb45eae11420e6e84b35903c179e016171ab38b
LDongGyu <ehdrbchd@naver.com>
2020-05-14 20:48:21 +0900

    네비게이션 드로어 에러 수정(정상 동작) ver.1

--- app/src/main/java/com/example/alarmdiary/MainActivity.kt
+++ app/src/main/java/com/example/alarmdiary/MainActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout:DrawerLayout
+    lateinit var drawerView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +33,19 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // 홈 버튼 활성화
         supportActionBar?.setHomeAsUpIndicator(R.drawable.menu_hamber) // 홈 버튼 이미지
         supportActionBar?.setDisplayShowTitleEnabled(false)
-//        val actionBar = supportActionBar
-//        actionBar?.setDisplayShowTitleEnabled(false)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+//        drawerView = findViewById(R.id.drawer)
+//        val actionBarDrawerToggle:ActionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,0,1)
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle)
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             android.R.id.home->{
-                drawer_layout.openDrawer(GravityCompat.START)
+                Toast.makeText(applicationContext,"Test",Toast.LENGTH_SHORT).show()
+                drawerLayout.openDrawer(GravityCompat.START)
             }
         }
 
