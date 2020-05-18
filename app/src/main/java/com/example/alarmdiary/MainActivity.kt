c53f91f7178abe3dd0ea1fc704df8f9f04ccac65
LDongGyu <ehdrbchd@naver.com>
2020-05-18 21:54:55 +0900

    style: ChartActivity 날짜 추가 및 Toolbar 오버플로우 메뉴 추가

--- app/src/main/java/com/example/alarmdiary/MainActivity.kt
+++ app/src/main/java/com/example/alarmdiary/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.alarmdiary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
@@ -66,10 +67,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_item,menu)
+
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             android.R.id.home->{
                 drawerLayout.openDrawer(GravityCompat.START)
+            }
+            R.id.etc->{
+                Toast.makeText(this,"기타 눌림",Toast.LENGTH_SHORT).show()
             }
         }
 
