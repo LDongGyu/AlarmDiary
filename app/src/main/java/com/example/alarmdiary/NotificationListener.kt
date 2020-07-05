7b9f5851e5ee6546267750e2493751b1793a1106
LDongGyu <ehdrbchd@naver.com>
2020-07-05 18:43:24 +0900

    fix: VectorDrawable 캐스팅 예외 처리

--- app/src/main/java/com/example/alarmdiary/NotificationListener.kt
+++ app/src/main/java/com/example/alarmdiary/NotificationListener.kt
@@ -3,10 +3,7 @@ package com.example.alarmdiary
 import android.app.Notification
 import android.content.ContentValues
 import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.DrawableWrapper
-import android.graphics.drawable.Icon
+import android.graphics.drawable.*
 import android.os.Build
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
@@ -26,7 +23,7 @@ class NotificationListener : NotificationListenerService() {
         val extras = sbn?.notification?.extras
         val title = extras?.getString(Notification.EXTRA_TITLE)
         val text = extras?.getCharSequence(Notification.EXTRA_TEXT)
-        val subText = extras?.getCharSequence(Notification.EXTRA_SUB_TEXT)
+
         var icon = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && notification != null) {
             if(notification?.getLargeIcon() != null){
                 notification?.getLargeIcon().loadDrawable(applicationContext)
@@ -38,7 +35,6 @@ class NotificationListener : NotificationListenerService() {
                 Icon.createWithResource(applicationContext,R.drawable.logo_color).loadDrawable(applicationContext)
             }
         } else {
-            TODO("VERSION.SDK_INT < P")
             Icon.createWithResource(applicationContext,R.drawable.logo_color).loadDrawable(applicationContext)
         }
 
@@ -52,30 +48,33 @@ class NotificationListener : NotificationListenerService() {
         val dbHelper = NotificationDbHelper(applicationContext)
         val db = dbHelper.writableDatabase
 
+//        Log.d("PushLog","onNotificationPosted ~ " +
+//                " title: " + title +
+//                " text : " + text +
+//                " date : " + dateTime[0] + " " + dateTime[1] +
+//                " packageName : " + sbn?.packageName +
+//                " icon : " + icon)
 
-        var bitmapDrawable = icon as BitmapDrawable
-        var bitmap = bitmapDrawable.bitmap
-        var stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream)
-        var bitmapdata = stream.toByteArray()
+        if(icon !is VectorDrawable){
+            var bitmapDrawable = icon as BitmapDrawable
+            var bitmap = bitmapDrawable.bitmap
+            var stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream)
+            var bitmapdata = stream.toByteArray()
 
-        val values = ContentValues().apply {
-            put(NotificationDbHelper.COLUMN_NAME_FROM,title)
-            put(NotificationDbHelper.COLUMN_NAME_CONTEXT,text.toString())
-            put(NotificationDbHelper.COLUMN_NAME_DATE,dateTime[0])
-            put(NotificationDbHelper.COLUMN_NAME_TIME,dateTime[1])
-            put(NotificationDbHelper.COLUMN_NAME_APP,sbn?.packageName)
-            put(NotificationDbHelper.COLUMN_NAME_ICON,bitmapdata)
+            val values = ContentValues().apply {
+                put(NotificationDbHelper.COLUMN_NAME_FROM,title)
+                put(NotificationDbHelper.COLUMN_NAME_CONTEXT,text.toString())
+                put(NotificationDbHelper.COLUMN_NAME_DATE,dateTime[0])
+                put(NotificationDbHelper.COLUMN_NAME_TIME,dateTime[1])
+                put(NotificationDbHelper.COLUMN_NAME_APP,sbn?.packageName)
+                put(NotificationDbHelper.COLUMN_NAME_ICON,bitmapdata)
+            }
+            if(!sbn?.packageName.equals("com.android.systemui") && notification?.getLargeIcon() != null && notification?.smallIcon != null ){
+                db?.insert(NotificationDbHelper.TABLE_NAME,null,values)
+            }
         }
-        if(!sbn?.packageName.equals("com.android.systemui") && notification?.getLargeIcon() != null && notification?.smallIcon != null ){
-            db?.insert(NotificationDbHelper.TABLE_NAME,null,values)
-        }
-        Log.d("PushLog","onNotificationPosted ~ " +
-                " packageName: " + sbn?.packageName +
-                " id: " + sbn?.id +
-                " postTime: " + sbn?.postTime +
-                " title: " + title +
-                " text : " + text +
-                " subText: " + subText)
+
+
     }
 }
